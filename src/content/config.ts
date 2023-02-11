71967550e428ba213a83353b181c3175ee9e8634
gaoqilin <3280743151@qq.com>
2023-02-11 15:02:26 +0800

    blog

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		'typora-root-url':z.string().optional().transform((str)=>{
+			return str ?  '/vue3' : undefined
+		})
+	}),
+});
+
+export const collections = { blog };
